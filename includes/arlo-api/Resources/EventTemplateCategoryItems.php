e1f3499f5f056c6b150e0103ad423f15303e2696
fentosi <fentosi.adam@gmail.com>
2016-07-05 14:13:42 +0200

    Fix categorytemplateitem sequence index error

--- includes/arlo-api/Resources/EventTemplateCategoryItems.php
+++ includes/arlo-api/Resources/EventTemplateCategoryItems.php
@@ -12,14 +12,16 @@ class EventTemplateCategoryItems extends Resource
 {
 	protected $apiPath = '/resources/eventtemplatecategoryitems/';
 	
-	public function search($fields = array(), $count = 20, $category_ids = array()) {
+	public function search($fields = array(), $count = 20, $category_ids = array(), $skip = 0) {
 		$data = array(
 			'fields=' . implode(',', $fields),
 			'categoryIDs='. implode(',', $category_ids),
 			'top=' . $count,
-			'format=json'
+			'format=json',
+			'skip=' . $skip,
+			'includeTotalCount=true'
 		);
-		
+				
 		$results = $this->request(implode('&', $data));
 		
 		return $results;
@@ -33,6 +35,29 @@ class EventTemplateCategoryItems extends Resource
 	}
 
 	public function getAllTemplateCategoriesItems($fields=array(), $category_ids = array()) {
-		return $this->search($fields, 1000)->Items;
+		$maxCount = 200;
+		$categoryCount = 32;
+		$items = [];
+				
+		$category_iteration = ceil(count($category_ids) / $categoryCount);
+				
+		for ($c = 0; $c < $category_iteration; $c++) {
+
+			$categories = array_slice($category_ids, $c * $categoryCount , $categoryCount);
+			
+			$result = $this->search($fields, $maxCount, $categories);
+						
+			$items = array_merge($items, $result->Items);
+			
+			if($result->TotalCount > $maxCount) {
+				$iterate = ceil($result->TotalCount/$maxCount)-1;// we've already gone once - minus 1
+				
+				for($i=1;$i<=$iterate;$i++) {
+					$items = array_merge($items, $this->search($fields, $maxCount, $categories, $i*$maxCount)->Items);
+				}
+			}	
+		}
+	
+		return $items;
 	}
 }
