b1de5158a90746b8a50098013bd947315ed3a910
fentosi <fentosi.adam@gmail.com>
2016-11-18 16:11:31 +1300

    Change Utilities::GUIDv4 to remove hyphens

--- includes/arlo-utilities.php
+++ includes/arlo-utilities.php
@@ -25,13 +25,15 @@ class Utilities {
 		return $now;    
     }
 
-	public static function GUIDv4 ($trim = true) {
+	public static function GUIDv4 ($trim = true, $remove_hyphens = false) {
+        
+
         // Windows
         if (function_exists('com_create_guid') === true) {
             if ($trim === true)
-                return trim(com_create_guid(), '{}');
+                $guid = trim(com_create_guid(), '{}');
             else
-                return com_create_guid();
+                $guid = com_create_guid();
         }
 
         // OSX/Linux
@@ -39,8 +41,14 @@ class Utilities {
             $data = openssl_random_pseudo_bytes(16);
             $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
             $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
-            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+            $guid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
         }
+
+        if ($remove_hyphens) {
+            return str_replace('-', '', $guid); 
+        }
+
+        return $guid;
     }
     
     public static function get_random_int() {
