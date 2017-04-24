ea031a56c9db45bf46719a4be648ff882a794893
fentosi <fentosi.adam@gmail.com>
2017-04-24 13:20:01 +1200

    Show timezone abbrev only if the WP and event's timezones are different

--- includes/shortcodes/arlo-events.php
+++ includes/shortcodes/arlo-events.php
@@ -845,7 +845,9 @@ private static function shortcode_event_filters($content = '', $atts = [], $shor
             $format = DateFormatter::date_format_to_strftime_format($format);
         }
 
-        if (!is_null($timezone) && ($timezone->getName() == $utc_timezone_name || $timezone->getName() != get_option('timezone_string') || (!empty($GLOBALS['selected_timezone_names']) && $GLOBALS['selected_timezone_names'] == $timezone->getName()) || $is_online) && preg_match('[I|M]', $format) === 1 && preg_match('[Z|z]', $format) === 0) {
+        $wp_timezone = new \DateTimeZone(get_option('timezone_string'));
+
+        if (!is_null($timezone) && ($timezone->getName() == $utc_timezone_name || $wp_timezone->getOffset($date) != $timezone->getOffset($date) || (!empty($GLOBALS['selected_timezone_names']) && $GLOBALS['selected_timezone_names'] == $timezone->getName()) || $is_online) && preg_match('[I|M]', $format) === 1 && preg_match('[Z|z]', $format) === 0) {
             $format .= " %Z";
         }        
 
