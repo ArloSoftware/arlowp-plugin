3cc1cbf107c0a50552f1d765cc4a657c29ec4313
Arlo Software Team <github@arlo.co>
2017-10-12 09:49:00 +1100

    Fix bug where filter changes to subcategories don't work, closes #208

--- includes/shortcodes/arlo-shortcodes.php
+++ includes/shortcodes/arlo-shortcodes.php
@@ -176,16 +176,25 @@ class Shortcodes {
 				);
 			}
 
+			$value_label = $item['string'];
+
+			$matches = array();
+			preg_match('~([&ndash;]*\s*)(.*)~',$value_label,$matches);
+
+			$value_prefix = $matches[1];
+			$value = $matches[2];
+
 			$is_hidden = false;
 			if (!empty($hidden_filters[$group][$type])) {
-				$is_hidden = in_array(esc_html($item['string']),$hidden_filters[$group][$type]);
+				$is_hidden = in_array(esc_html($value),$hidden_filters[$group][$type]);
 			}
 
 			if (!$is_hidden) {
 				$value_label = $item['string'];
 
-				if (!empty(get_option('arlo_filter_settings')[$group][$type][htmlspecialchars($value_label)])) {
-					$value_label = get_option('arlo_filter_settings')[$group][$type][htmlspecialchars($value_label)];
+
+				if (!empty(get_option('arlo_filter_settings')[$group][$type][htmlspecialchars($value)])) {
+					$value_label = $value_prefix . get_option('arlo_filter_settings')[$group][$type][htmlspecialchars($value)];
 				}
 
                 $selected = (strlen($selected_value) && strtolower($selected_value) == strtolower($item['value'])) ? ' selected="selected"' : '';
