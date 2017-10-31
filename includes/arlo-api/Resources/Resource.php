c6c872c59417f3118b32bba225753da57b5b84f8
fentosi <fentosi.adam@gmail.com>
2017-11-01 12:28:31 +1300

    Eliminate PHP notices

--- includes/arlo-api/Resources/Resource.php
+++ includes/arlo-api/Resources/Resource.php
@@ -30,12 +30,15 @@ class Resource
             return $this->data[$name];
         }
 
-        $trace = debug_backtrace();
-        trigger_error(
-            'Undefined property via __get(): ' . $name .
-            ' in ' . $trace[0]['file'] .
-            ' on line ' . $trace[0]['line'],
-            E_USER_NOTICE);
+        if (WP_DEBUG) {
+            $trace = debug_backtrace();
+            trigger_error(
+                'Undefined property via __get(): ' . $name .
+                ' in ' . $trace[0]['file'] .
+                ' on line ' . $trace[0]['line'],
+                E_USER_NOTICE);    
+        }
+
         return null;
     }
     
