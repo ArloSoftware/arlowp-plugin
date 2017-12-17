f63da85cbeeb3bffacac2cd81da6d2b339004616
fentosi <fentosi.adam@gmail.com>
2017-12-18 12:49:39 +1300

    Bump version to 3.6

--- arlo-for-wordpress.php
+++ arlo-for-wordpress.php
@@ -14,7 +14,7 @@
  * @wordpress-plugin
  * Plugin Name:       Arlo
  * Description:       Connect your WordPress to Arlo
- * Version:           3.6
+ * Version:           3.7
  * Author:            Arlo
  * Author URI:       http://arlo.co
  * Text Domain:       arlo-for-wordpress
