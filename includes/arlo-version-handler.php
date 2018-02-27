fe85cf89b00dc28d8656b0aee941a154d08ed160
Gabriel Oheix <gabriel.oheix@arlo.co>
2018-02-27 15:48:15 +1300

    Fix: Import is failing while importing timezones for Arrampica #334

--- includes/arlo-version-handler.php
+++ includes/arlo-version-handler.php
@@ -187,12 +187,26 @@ class VersionHandler {
 			break;	
 
 			case '3.6':
-			$exists = $this->dbl->get_var("SHOW COLUMNS FROM " . $this->dbl->prefix . "arlo_events LIKE 'e_is_taxexempt'", 0, 0);
-			if (is_null($exists)) {
-				$this->dbl->query("ALTER TABLE " . $this->dbl->prefix . "arlo_events ADD e_is_taxexempt TINYINT(1) NOT NULL DEFAULT '0' AFTER e_isonline");
-				$this->dbl->query("ALTER TABLE " . $this->dbl->prefix . "arlo_events ADD INDEX (e_is_taxexempt(1))");
-			}
-			break;					
+				$exists = $this->dbl->get_var("SHOW COLUMNS FROM " . $this->dbl->prefix . "arlo_events LIKE 'e_is_taxexempt'", 0, 0);
+				if (is_null($exists)) {
+					$this->dbl->query("ALTER TABLE " . $this->dbl->prefix . "arlo_events ADD e_is_taxexempt TINYINT(1) NOT NULL DEFAULT '0' AFTER e_isonline");
+					$this->dbl->query("ALTER TABLE " . $this->dbl->prefix . "arlo_events ADD INDEX (e_is_taxexempt(1))");
+				}
+			break;
+
+			case '3.6.1':
+				$this->dbl->query("ALTER TABLE " . $this->dbl->prefix . "arlo_events DROP e_timezone_id;");
+				$this->dbl->query("DROP TABLE " . $this->dbl->prefix . "arlo_timezones;");
+				$this->dbl->query("CREATE TABLE " . $this->dbl->prefix . "arlo_timezones (
+					id int(11) NOT NULL,
+					name varchar(256) NOT NULL,
+					windows_tz_id varchar(256) NOT NULL,
+					import_id int(10) unsigned NOT NULL,
+					PRIMARY KEY  (id, import_id)) 
+					CHARACTER SET " . $this->dbl->charset . (!empty($this->dbl->collate) ? " COLLATE=" . $this->dbl->collate  : "") . ";
+				");
+				$this->dbl->query("ALTER TABLE " . $this->dbl->prefix . "arlo_events ADD e_timezone_id INT(11) NULL AFTER e_timezone");
+			break;
 		}
 	}	
 	
