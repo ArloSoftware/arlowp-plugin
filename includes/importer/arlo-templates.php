2dc428ce801d69d334b66a12cc1d4554a471e4d6
Gabriel Oheix <gabriel.oheix@arlo.co>
2018-04-12 11:52:05 +1200

    New: Import RegisterPrivateInterestUri #360

--- includes/importer/arlo-templates.php
+++ includes/importer/arlo-templates.php
@@ -22,8 +22,8 @@ class Templates extends BaseImporter {
 			$query = $this->dbl->query(
 				$this->dbl->prepare( 
 					"INSERT INTO " . $this->table_name ." 
-					(et_arlo_id, et_code, et_name, et_descriptionsummary, et_advertised_duration, et_post_name, et_post_id, import_id, et_registerinteresturi, et_viewuri, et_region) 
-					VALUES ( %d, %s, %s, %s, %s, %s, %d, %s, %s, %s, %s) 
+					(et_arlo_id, et_code, et_name, et_descriptionsummary, et_advertised_duration, et_post_name, et_post_id, import_id, et_registerinteresturi, et_registerprivateinteresturi, et_viewuri, et_region) 
+					VALUES ( %d, %s, %s, %s, %s, %s, %d, %s, %s, %s, %s, %s) 
 					", 
 					$item->TemplateID,
 					@$item->Code,
@@ -34,6 +34,7 @@ class Templates extends BaseImporter {
 					$post_id,
 					$this->import_id,
 					!empty($item->RegisterInterestUri) ? $item->RegisterInterestUri : '',
+					!empty($item->RegisterPrivateInterestUri) ? $item->RegisterPrivateInterestUri : '',
 					!empty($item->ViewUri) ? $item->ViewUri : '',
 					(!empty($item->Region) ? $item->Region : '')
 				)
