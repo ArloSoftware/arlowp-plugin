5957b8684f0728e101dd0e04c3b372142738d7c8
Ryan Halliday <ryan.halliday@arlo.co>
2019-01-08 14:51:35 +1300

    Remove cookies/sessions to not confuse redirect on new shortcode #407

--- admin/includes/class-arlo-for-wordpress-settings.php
+++ admin/includes/class-arlo-for-wordpress-settings.php
@@ -156,16 +156,26 @@ class Arlo_For_Wordpress_Settings {
 
 			if ( array_key_exists('arlo-new-custom-shortcode', $_COOKIE) ) {
 				$custom_shortcode = $_COOKIE['arlo-new-custom-shortcode'];
+
+				// Remove all cookies... 
+				// Old code has setup where cookies are very temporary. JS deletes them too. 
 				unset( $_COOKIE['arlo-new-custom-shortcode'] );
 				setcookie('arlo-new-custom-shortcode', null, -1, '/', $domain);
+				unset($_COOKIE['arlo-nav-tab']);
+				unset($_COOKIE['arlo-vertical-tab']);
+				setcookie('arlo-nav-tab', null, -1, '/', $domain);
+				setcookie('arlo-vertical-tab', null, -1, '/', $domain);
 
 				//some shared hosting doesn't delete the cookie properly
 				if (!isset($_SESSION['arlo-new-custom-shortcode'])) {
 					$_SESSION['arlo-new-custom-shortcode'] = true;
 					wp_redirect( admin_url('admin.php?page=arlo-for-wordpress#pages/' . $custom_shortcode) );
+					exit;
 				} else {
 					unset($_SESSION['arlo-new-custom-shortcode']);
 				}
+			} else if (isset($_SESSION['arlo-new-custom-shortcode'])){
+				unset($_SESSION['arlo-new-custom-shortcode']);
 			}
 
 			if ( array_key_exists('arlo-nav-tab', $_COOKIE) ) {
@@ -177,6 +187,7 @@ class Arlo_For_Wordpress_Settings {
 				if (!isset($_SESSION['arlo-nav-tab'])) {
 					$_SESSION['arlo-nav-tab'] = true;
 					wp_redirect( admin_url('admin.php?page=arlo-for-wordpress#' . $nav_tab) );
+					exit;
 				} else {
 					unset($_SESSION['arlo-nav-tab']);
 				}
@@ -191,6 +202,7 @@ class Arlo_For_Wordpress_Settings {
 				if (!isset($_SESSION['arlo-vertical-tab'])) {
 					$_SESSION['arlo-vertical-tab'] = true;
 					wp_redirect( admin_url('admin.php?page=arlo-for-wordpress#pages/' . $page) );
+					exit;
 				} else {
 					unset($_SESSION['arlo-vertical-tab']);
 				}
